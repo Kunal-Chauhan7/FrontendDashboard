da609e2337d85e2337d18282c017269aae6bf664
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-07-29 00:15:45 +0530

    done making banner of the main component

--- src/Components/MainComponent.jsx
+++ src/Components/MainComponent.jsx
@@ -1,8 +1,27 @@
 import './MainComponent.css';
+import dashboardImage from '../assets/MainComponent.png'
 const MainComponent = () => {
   return (
     <div className='Container'>
-      Main Component
+      <h2>My Dashboard</h2>
+      <div className='WelcomeBanner'>
+        <div className='WelcomeBannerInner'>
+          <h2>Welcome Back ,</h2>
+          <h1>Annabeth Chase</h1>
+          <div className='spanDiv'>
+            <span>You've learned 78% of your goal this week. <br /> Continue learning and improve your progress</span>
+          </div>
+        </div>
+        <img src={dashboardImage} alt=""/>
+      </div>
+      <div>
+        <h2>Your Courses</h2>
+        <div className='CardContainer'>
+          <div>
+            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
